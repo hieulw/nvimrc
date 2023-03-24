2935404f68415d552f8ecb2b6038236f3df8eb69
Hieu Le <hieu.le@fiisoft.com>
2023-03-24 18:09:53 +0700

    update: neovim 8.3

--- modules/git.lua
+++ modules/git.lua
@@ -64,7 +64,7 @@ require('gitsigns').setup {
         ['o ih'] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>',
         ['x ih'] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>'
     },
-    watch_index = {interval = 1000, follow_files = true},
+    -- watch_index = {interval = 1000, follow_files = true},
     attach_to_untracked = true,
     current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
     current_line_blame_opts = {
