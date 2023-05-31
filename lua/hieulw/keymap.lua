b5d93c219bda7d074f3e5da020511fa6910841f2
Hieu Le <hieu.le@fiisoft.com>
2023-05-31 15:45:41 +0700

    remove redundant keymap

--- lua/hieulw/keymap.lua
+++ lua/hieulw/keymap.lua
@@ -3,9 +3,6 @@ vim.g.localleader = ' '
 
 vim.keymap.set({'n', 'v'}, vim.g.mapleader, '<Nop>', {silent = true})
 
--- My hand too lazy to reach Esc
-vim.keymap.set('i', 'kj', '<esc>', {silent = true, nowait = true})
-
 -- nano feel here
 vim.keymap.set('n', '<C-q>', ':q<cr>', {silent = true})
 vim.keymap.set('n', '<C-s>', ':w<cr>', {silent = true})
@@ -16,22 +13,6 @@ vim.keymap.set('v', 'H', "<gv")
 vim.keymap.set('v', 'J', ":m '>+1<cr>gv=gv")
 vim.keymap.set('v', 'K', ":m '<-2<cr>gv=gv")
 
--- Better window navigation
--- vim.keymap.set('n', '<C-h>', ":wincmd h<cr>", {silent = true})
--- vim.keymap.set('n', '<C-j>', ":wincmd j<cr>", {silent = true})
--- vim.keymap.set('n', '<C-k>', ":wincmd k<cr>", {silent = true})
--- vim.keymap.set('n', '<C-l>', ":wincmd l<cr>", {silent = true})
--- Resize windows
-vim.keymap.set('n', '<C-Down>', ":resize -2<CR>", {silent = true})
-vim.keymap.set('n', '<C-Up>', ":resize +2<CR>", {silent = true})
-vim.keymap.set('n', '<C-Left>', ":vertical resize -2<CR>", {silent = true})
-vim.keymap.set('n', '<C-Right>', ":vertical resize +2<CR>", {silent = true})
-if vim.fn.has("macunix") then
-    vim.keymap.set('n', '˚', ":resize -2<CR>", {silent = true})
-    vim.keymap.set('n', '∆', ":resize +2<CR>", {silent = true})
-    vim.keymap.set('n', '¬', ":vertical resize -2<CR>", {silent = true})
-    vim.keymap.set('n', '˙', ":vertical resize +2<CR>", {silent = true})
-end
 -- Switch between vertical and horizontal
 vim.keymap.set('n', '<leader>sh', "<C-w>t<C-w>K")
 vim.keymap.set('n', '<leader>sv', "<C-w>t<C-w>H")
@@ -48,20 +29,11 @@ vim.keymap.set('n', '<leader>gd', ":Gvdiff!<CR>")
 vim.keymap.set('n', '<leader>gdh', ":diffget //2<CR>")
 vim.keymap.set('n', '<leader>gdl', ":diffget //3<CR>")
 
--- Functions
-vim.keymap.set('n', '<leader>e', ":Explore<cr>")
-vim.keymap.set('n', '<leader>gb', ":Git blame<CR>")
-vim.keymap.set('n', '<leader>d', ":DBUIToggle<CR>")
-
 -- Diagnostic
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev,
                {desc = "Go to previous diagnostic message"})
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next,
                {desc = "Go to next diagnostic message"})
-
--- Easy Align
-vim.keymap.set('x', 'ga', "<Plug>(EasyAlign)")
-vim.keymap.set('n', 'ga', "<Plug>(EasyAlign)")
 
 -- Apply @record to multiple lines
 -- https://medium.com/@schtoeffel/you-don-t-need-more-than-one-cursor-in-vim-2c44117d51db
@@ -98,15 +70,11 @@ vim.keymap.set('n', 'gx', function(path)
 end)
 
 -- Keep cursor center
-vim.keymap.set('n', 'n', "nzzzv")
-vim.keymap.set('n', 'N', "Nzzzv")
-vim.keymap.set('n', 'J', "mzJ`z")
-vim.keymap.set('n', '<C-i>', "<C-i>zzzv")
-vim.keymap.set('n', '<C-o>', "<C-o>zzzv")
-
--- Visual text process
--- vim.keymap.set('v', 'gj', ":join<cr>")
--- vim.keymap.set('v', 'gs', ":sort<cr>")
+-- vim.keymap.set('n', 'n', "nzzzv")
+-- vim.keymap.set('n', 'N', "Nzzzv")
+-- vim.keymap.set('n', 'J', "mzJ`z")
+-- vim.keymap.set('n', '<C-i>', "<C-i>zzzv")
+-- vim.keymap.set('n', '<C-o>', "<C-o>zzzv")
 
 -- Undo breakpoint
 vim.keymap.set('i', ',', ",<c-g>u")
