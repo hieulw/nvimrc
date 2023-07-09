dc11cbe37cf76b4ecb2b1d38869e8de89e65b47b
Hieu Le <hieu.le@fiisoft.com>
2023-07-09 14:53:33 +0700

    improve sort and format

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -21,6 +21,7 @@ return {
       local border_opts = {
         border = "single",
         winhighlight = "Normal:Normal,FloatBorder:FloatBorder,CursorLine:Visual,Search:None",
+        scrolloff = 2,
       }
       local has_words_before = function()
         unpack = unpack or table.unpack
@@ -37,8 +38,18 @@ return {
         end,
         preselect = cmp.PreselectMode.None,
         formatting = {
-          format = function(_, item)
+          format = function(entry, item)
             item.kind = string.format("%s %s", kind_icons[item.kind], item.kind)
+            item.menu = ({
+              nvim_lsp = "[lsp]",
+              nvim_lsp_signature_help = "[signature]",
+              nvim_lsp_document_symbol = "[symbol]",
+              vsnip = "[vsnip]",
+              buffer = "[buffer]",
+              path = "[path]",
+              cmdline = nil,
+            })[entry.source.name]
+            item.abbr = item.abbr:match("[^(]+") -- fn(...args) -> fn
             return item
           end,
         },
@@ -79,8 +90,13 @@ return {
           ["<C-e>"] = cmp.mapping({ i = cmp.mapping.abort(), c = cmp.mapping.close() }),
           ["<CR>"] = cmp.mapping.confirm({ select = false }),
           ["<C-j>"] = cmp.mapping(function(fallback)
-            if cmp.visible() and cmp.get_selected_entry() then
-              cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+            if cmp.visible() then
+              if cmp.get_selected_entry() then
+                cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+              else
+                cmp.select_next_item()
+                cmp.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false })
+              end
             elseif vim.fn["vsnip#available"](1) == 1 then
               feedkey("<Plug>(vsnip-expand-or-jump)", "")
             else
@@ -99,12 +115,20 @@ return {
         }),
         sources = cmp.config.sources({
           { name = "nvim_lsp", priority = 1000 },
-          { name = "nvim_lua", priority = 1000 },
           { name = "nvim_lsp_signature_help" },
           { name = "vsnip", priority = 750 },
           { name = "buffer", priority = 500 },
           { name = "path", priority = 250 },
         }),
+        sorting = {
+          comparators = {
+            cmp.config.compare.exact,
+            cmp.config.compare.kind,
+            cmp.config.compare.recently_used,
+            cmp.config.compare.scopes,
+          },
+        },
+        view = { entries = { name = "custom", selection_order = "near_cursor" } },
       })
       cmp.setup.cmdline({ "/", "?" }, {
         mapping = cmp.mapping.preset.cmdline(),
@@ -113,6 +137,7 @@ return {
       cmp.setup.cmdline(":", {
         mapping = cmp.mapping.preset.cmdline(),
         sources = cmp.config.sources({ { name = "path" } }, { { name = "cmdline" } }),
+        view = { entries = { name = "wildmenu", separator = " " } },
       })
     end,
   },
