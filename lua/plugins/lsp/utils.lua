0ec52c39211479257e6cb32af403f0e329280772
Hieu Le <hieu.le@fiisoft.com>
2023-12-20 16:42:32 +0700

    folding using treesitter instead of lsp

--- lua/plugins/lsp/utils.lua
+++ lua/plugins/lsp/utils.lua
@@ -61,10 +61,6 @@ end
 function M.capabilities()
   local capabilities = vim.lsp.protocol.make_client_capabilities()
   capabilities.textDocument.completion.completionItem.snippetSupport = true
-  capabilities.textDocument.foldingRange = {
-    dynamicRegistration = false,
-    lineFoldingOnly = true,
-  }
   return require("cmp_nvim_lsp").default_capabilities(capabilities)
 end
 
--- lua/plugins/utility.lua
+++ lua/plugins/utility.lua
@@ -97,26 +97,8 @@ return {
           scrollD = "<C-d>",
         },
       },
-      provider_selector = function(_, filetype, buftype)
-        local function handleFallbackException(bufnr, err, providerName)
-          if type(err) == "string" and err:match("UfoFallbackException") then
-            return require("ufo").getFolds(bufnr, providerName)
-          else
-            return require("promise").reject(err)
-          end
-        end
-
-        return (filetype == "" or buftype == "nofile") and "indent" -- only use indent until a file is opened
-          or function(bufnr)
-            return require("ufo")
-              .getFolds(bufnr, "lsp")
-              :catch(function(err)
-                return handleFallbackException(bufnr, err, "treesitter")
-              end)
-              :catch(function(err)
-                return handleFallbackException(bufnr, err, "indent")
-              end)
-          end
+      provider_selector = function()
+        return { "treesitter", "indent" }
       end,
     },
   },
