01d5d17839726629e31abc57ead78ac6b1193945
Hieu Le <hieulw99@gmail.com>
2023-12-27 08:31:59 +0700

    remove last loc autocmd

--- lua/hieulw/autocmds.lua
+++ lua/hieulw/autocmds.lua
@@ -121,20 +121,3 @@ autocmd("BufWinEnter", {
     vim.cmd("silent! loadview 1")
   end,
 })
--- autocmd("BufReadPost", {
---   desc = "go to last loc when opening a buffer",
---   group = augroup("last_loc"),
---   callback = function(event)
---     local exclude = { "gitcommit" }
---     local buf = event.buf
---     if vim.tbl_contains(exclude, vim.bo[buf].filetype) or vim.b[buf].lazyvim_last_loc then
---       return
---     end
---     vim.b[buf].lazyvim_last_loc = true
---     local mark = vim.api.nvim_buf_get_mark(buf, '"')
---     local lcount = vim.api.nvim_buf_line_count(buf)
---     if mark[1] > 0 and mark[1] <= lcount then
---       pcall(vim.api.nvim_win_set_cursor, 0, mark)
---     end
---   end,
--- })
