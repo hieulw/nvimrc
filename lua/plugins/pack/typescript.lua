cc3f6e67ca00d7dffdce5f9a14edc013d75157fa
Hieu Le <hieulw99@gmail.com>
2024-01-01 22:05:23 +0700

    biome to eslint

--- lua/plugins/pack/typescript.lua
+++ lua/plugins/pack/typescript.lua
@@ -2,14 +2,26 @@ return {
   {
     "williamboman/mason.nvim",
     opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, { "typescript-language-server", "js-debug-adapter" })
+      vim.list_extend(opts.ensure_installed, {
+        "typescript-language-server",
+        "eslint-lsp",
+        -- "biome",
+      })
+    end,
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    opts = function(_, opts)
+      local nls = require("null-ls")
+      vim.list_extend(opts.sources, {
+        -- nls.builtins.formatting.biome,
+      })
     end,
   },
   {
     "pmizio/typescript-tools.nvim",
     opts = {
       tsserver_file_preferences = {
-        -- Inlay Hints
         includeInlayParameterNameHints = "all",
         includeInlayParameterNameHintsWhenArgumentMatchesName = true,
         includeInlayFunctionParameterTypeHints = true,
@@ -56,41 +68,32 @@ return {
     end,
   },
   {
-    "nvimtools/none-ls.nvim",
-    opts = function(_, opts)
-      local nls = require("null-ls")
-      vim.list_extend(opts.sources, { nls.builtins.formatting.biome })
-    end,
-  },
-  {
     "neovim/nvim-lspconfig",
     dependencies = { "pmizio/typescript-tools.nvim" },
     opts = {
-      -- make sure mason installs the server
       servers = {
-        -- ESLint
-        -- eslint = {
-        --   settings = {
-        --     -- helps eslint find the eslintrc when it's placed in a subfolder instead of the cwd root
-        --     -- workingDirectory = { mode = "auto" },
-        --   },
-        -- },
-        biome = {},
+        eslint = {
+          settings = {
+            -- helps eslint find the eslintrc when it's placed in a subfolder instead of the cwd root
+            workingDirectory = { mode = "auto" },
+          },
+        },
+        -- biome = {},
       },
       setup = {
-        -- eslint = function()
-        --   vim.api.nvim_create_autocmd("BufWritePre", {
-        --     callback = function(event)
-        --       local client = vim.lsp.get_active_clients({ bufnr = event.buf, name = "eslint" })[1]
-        --       if client then
-        --         local diag = vim.diagnostic.get(event.buf, { namespace = vim.lsp.diagnostic.get_namespace(client.id) })
-        --         if #diag > 0 then
-        --           vim.cmd("EslintFixAll")
-        --         end
-        --       end
-        --     end,
-        --   })
-        -- end,
+        eslint = function()
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            callback = function(event)
+              local client = vim.lsp.get_clients({ bufnr = event.buf, name = "eslint" })[1]
+              if client then
+                local diag = vim.diagnostic.get(event.buf, { namespace = vim.lsp.diagnostic.get_namespace(client.id) })
+                if #diag > 0 then
+                  vim.cmd("EslintFixAll")
+                end
+              end
+            end,
+          })
+        end,
         tsserver = function()
           -- skip mason-lspconfig auto setup
           return true
@@ -98,129 +101,4 @@ return {
       },
     },
   },
-  -- {
-  --   "mfussenegger/nvim-dap",
-  --   opts = {
-  --     setup = {
-  --       vscode_js_debug = function()
-  --         local function get_js_debug()
-  --           local install_path = require("mason-registry").get_package("js-debug-adapter"):get_install_path()
-  --           return install_path .. "/js-debug/src/dapDebugServer.js"
-  --         end
-  --
-  --         for _, adapter in ipairs({ "pwa-node", "pwa-chrome", "pwa-msedge", "node-terminal", "pwa-extensionHost" }) do
-  --           require("dap").adapters[adapter] = {
-  --             type = "server",
-  --             host = "localhost",
-  --             port = "${port}",
-  --             executable = {
-  --               command = "node",
-  --               args = {
-  --                 get_js_debug(),
-  --                 "${port}",
-  --               },
-  --             },
-  --           }
-  --         end
-  --
-  --         for _, language in ipairs({ "typescript", "javascript" }) do
-  --           require("dap").configurations[language] = {
-  --             {
-  --               type = "pwa-node",
-  --               request = "launch",
-  --               name = "Launch file",
-  --               program = "${file}",
-  --               cwd = "${workspaceFolder}",
-  --             },
-  --             {
-  --               type = "pwa-node",
-  --               request = "attach",
-  --               name = "Attach",
-  --               processId = require("dap.utils").pick_process,
-  --               cwd = "${workspaceFolder}",
-  --             },
-  --             {
-  --               type = "pwa-node",
-  --               request = "launch",
-  --               name = "Debug Jest Tests",
-  --               -- trace = true, -- include debugger info
-  --               runtimeExecutable = "node",
-  --               runtimeArgs = {
-  --                 "./node_modules/jest/bin/jest.js",
-  --                 "--runInBand",
-  --               },
-  --               rootPath = "${workspaceFolder}",
-  --               cwd = "${workspaceFolder}",
-  --               console = "integratedTerminal",
-  --               internalConsoleOptions = "neverOpen",
-  --             },
-  --             {
-  --               type = "pwa-chrome",
-  --               name = "Attach - Remote Debugging",
-  --               request = "attach",
-  --               program = "${file}",
-  --               cwd = vim.fn.getcwd(),
-  --               sourceMaps = true,
-  --               protocol = "inspector",
-  --               port = 9222, -- Start Chrome google-chrome --remote-debugging-port=9222
-  --               webRoot = "${workspaceFolder}",
-  --             },
-  --             {
-  --               type = "pwa-chrome",
-  --               name = "Launch Chrome",
-  --               request = "launch",
-  --               url = "http://localhost:5173", -- This is for Vite. Change it to the framework you use
-  --               webRoot = "${workspaceFolder}",
-  --               userDataDir = "${workspaceFolder}/.vscode/vscode-chrome-debug-userdatadir",
-  --             },
-  --           }
-  --         end
-  --
-  --         for _, language in ipairs({ "typescriptreact", "javascriptreact" }) do
-  --           require("dap").configurations[language] = {
-  --             {
-  --               type = "pwa-chrome",
-  --               name = "Attach - Remote Debugging",
-  --               request = "attach",
-  --               program = "${file}",
-  --               cwd = vim.fn.getcwd(),
-  --               sourceMaps = true,
-  --               protocol = "inspector",
-  --               port = 9222, -- Start Chrome google-chrome --remote-debugging-port=9222
-  --               webRoot = "${workspaceFolder}",
-  --             },
-  --             {
-  --               type = "pwa-chrome",
-  --               name = "Launch Chrome",
-  --               request = "launch",
-  --               url = "http://localhost:5173", -- This is for Vite. Change it to the framework you use
-  --               webRoot = "${workspaceFolder}",
-  --               userDataDir = "${workspaceFolder}/.vscode/vscode-chrome-debug-userdatadir",
-  --             },
-  --           }
-  --         end
-  --       end,
-  --     },
-  --   },
-  -- },
-  -- {
-  --   "nvim-neotest/neotest",
-  --   dependencies = {
-  --     "nvim-neotest/neotest-jest",
-  --     "marilari88/neotest-vitest",
-  --     "thenbe/neotest-playwright",
-  --   },
-  --   opts = function(_, opts)
-  --     vim.list_extend(opts.adapters, {
-  --       require("neotest-jest"),
-  --       require("neotest-vitest"),
-  --       require("neotest-playwright").adapter({
-  --         options = {
-  --           persist_project_selection = true,
-  --           enable_dynamic_test_discovery = true,
-  --         },
-  --       }),
-  --     })
-  --   end,
-  -- },
 }
