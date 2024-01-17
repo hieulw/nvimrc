4870c3db4bbdaf1f69202c55d7b229aca90a03a3
hieulw <murindeventa@gmail.com>
2024-01-17 20:28:24 +0700

    add database

---
+++ lua/plugins/database.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "kndndrj/nvim-dbee",
+    build = function()
+      require("dbee").install()
+    end,
+    opts = {
+      extra_helpers = {
+        ["postgres"] = {
+          ["List All"] = "select * from {{ .Table }}",
+        },
+      },
+      drawer = {
+        disable_help = true,
+      },
+    },
+    keys = function()
+      local dbee = require("dbee")
+      return {
+        { "<leader>db", dbee.toggle, mode = "n", desc = "Open database UI" },
+      }
+    end,
+  },
+}
