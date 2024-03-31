ce05f3378cdadb223ce75e9d565cdc236ea840c6
hieulw <murindeventa@gmail.com>
2024-03-31 20:11:57 +0700

    add lua DAP adapter

--- lua/plugins/pack/lua.lua
+++ lua/plugins/pack/lua.lua
@@ -70,4 +70,59 @@ return {
       },
     },
   },
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      {
+        "jbyuki/one-small-step-for-vimkind",
+        keys = {
+          {
+            "<leader>dx",
+            function()
+              require("osv").launch({ port = 8086 })
+            end,
+            desc = "Launch Lua adapter",
+          },
+        },
+      },
+    },
+    opts = {
+      configurations = {
+        lua = {
+          {
+            type = "nlua",
+            request = "attach",
+            name = "Run this file",
+            start_neovim = true,
+          },
+          {
+            type = "nlua",
+            request = "attach",
+            name = "Attach to running Neovim instance",
+            port = 8086,
+          },
+        },
+      },
+      adapters = {
+        nlua = function(callback, config)
+          local dap, osv = require("dap"), require("osv")
+          local adapter = {
+            type = "server",
+            host = config.host or "127.0.0.1",
+            port = config.port or 8086,
+          }
+          if config.start_neovim then
+            local dap_run = dap.run
+            dap.run = function(c)
+              adapter.port = c.port
+              adapter.host = c.host
+            end
+            osv.run_this()
+            dap.run = dap_run
+          end
+          callback(adapter)
+        end,
+      },
+    },
+  },
 }
