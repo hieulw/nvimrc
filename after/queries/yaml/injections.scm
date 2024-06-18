92c89428730c21a04d40b6beaa51b539962b00c2
hieu.lequang3 <hieu.lequang3@vib.com.vn>
2024-06-18 08:55:12 +0700

    add: bash highlight for azure-pipelines.yaml

---
+++ after/queries/yaml/injections.scm
@@ -0,0 +1,26 @@
+(block_mapping_pair
+  key: (flow_node) @_run
+  (#any-of? @_run "run" "bash" "script" "before_script" "after_script")
+  value: (flow_node
+    (plain_scalar
+      (string_scalar) @injection.content)
+    (#set! injection.language "bash")))
+
+(block_mapping_pair
+  key: (flow_node) @_run
+  (#any-of? @_run "run" "bash" "script" "before_script" "after_script")
+  value: (block_node
+    (block_scalar) @injection.content
+    (#set! injection.language "bash")
+    (#offset! @injection.content 0 1 0 0)))
+
+(block_mapping_pair
+  key: (flow_node) @_run
+  (#any-of? @_run "run" "bash" "script" "before_script" "after_script")
+  value: (block_node
+    (block_sequence
+      (block_sequence_item
+        (flow_node
+          (plain_scalar
+            (string_scalar) @injection.content))
+        (#set! injection.language "bash")))))
