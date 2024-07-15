ae1609f4e892c027c3072f52b66ff4d2f6398945
Le Hieu <murindeventa@gmail.com>
2024-07-15 10:43:36 +0700

    fix: treesitter config order

--- lua/plugins/treesitter/config.lua
+++ lua/plugins/treesitter/config.lua
@@ -3,9 +3,9 @@ local M = {}
 function M.setup(_, opts)
   ---@see https://github.com/nvim-treesitter/nvim-treesitter/issues/3581
   -- commenting out the comment queries in injections.scm seem to fix performance issues
-  require("nvim-treesitter.configs").setup(opts)
   require("plugins.treesitter.keymaps").setup()
   require("plugins.treesitter.parser").setup()
+  require("nvim-treesitter.configs").setup(opts)
   require("ts-comments").setup()
 end
 
