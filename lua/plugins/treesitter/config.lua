fdac28746842544176b200accc8ad618f7cf6fbf
Le Hieu <murindeventa@gmail.com>
2026-04-13 16:43:29 +0300

    feat(treesitter): register custom parsers and highlight startup

--- lua/plugins/treesitter/config.lua
+++ lua/plugins/treesitter/config.lua
@@ -1,11 +1,24 @@
 local M = {}
 
+local function setup_treesitter_highlight()
+  local group = vim.api.nvim_create_augroup("hieulw_treesitter_highlight", { clear = true })
+  vim.api.nvim_create_autocmd("FileType", {
+    group = group,
+    callback = function(args)
+      if vim.bo[args.buf].buftype ~= "" then
+        return
+      end
+      pcall(vim.treesitter.start, args.buf)
+    end,
+  })
+end
+
 function M.setup(_, opts)
   ---@see https://github.com/nvim-treesitter/nvim-treesitter/issues/3581
   -- commenting out the comment queries in injections.scm seem to fix performance issues
   require("plugins.treesitter.keymaps").setup()
   require("plugins.treesitter.parser").setup()
-  require("plugins.treesitter.compat").setup()
+  -- require("plugins.treesitter.compat").setup()
   require("nvim-treesitter").setup({ install_dir = opts.install_dir })
   local parsers = vim.list.unique(opts.parsers or {})
   if #parsers > 0 then
@@ -17,6 +30,7 @@ function M.setup(_, opts)
       require("nvim-treesitter").install(missing)
     end
   end
+  setup_treesitter_highlight()
   require("ts-comments").setup()
 end
 
--- lua/plugins/treesitter/parser.lua
+++ lua/plugins/treesitter/parser.lua
@@ -9,19 +9,36 @@ local parsers = {
     },
     filetype = "blade",
   },
+  jsonc = {
+    install_info = {
+      url = "https://github.com/sunilunnithan/tree-sitter-jsonc",
+      files = { "src/parser.c" },
+      branch = "main",
+    },
+    filetype = "jsonc",
+  },
 }
 
-function M.setup()
+local function register_custom_parsers()
   local parser_config = require("nvim-treesitter.parsers")
   if type(parser_config.get_parser_configs) == "function" then
     parser_config = parser_config.get_parser_configs()
   end
 
   for parser, config in pairs(parsers) do
-    if parser_config[parser] == nil then
-      parser_config[parser] = config
-    end
+    parser_config[parser] = config
   end
+end
+
+function M.setup()
+  register_custom_parsers()
+
+  local group = vim.api.nvim_create_augroup("user_treesitter_custom_parsers", { clear = true })
+  vim.api.nvim_create_autocmd("User", {
+    group = group,
+    pattern = "TSUpdate",
+    callback = register_custom_parsers,
+  })
 end
 
 return M
